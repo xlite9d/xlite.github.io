253d3d13c9bfc746c708380c8fe15b3d56ff67cd
14m0r81t100 <159073116+1056516@users.noreply.github.com>
2025-04-22 21:21:35 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://vplaza.org/bare',
+    bare:'/bare',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
