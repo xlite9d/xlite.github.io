f2f280608aa3b6f6dc829eaec737ab793d903e09
YAsdgfterf <152664904+YAsdgfterf@users.noreply.github.com>
2025-04-23 20:35:45 -0700

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://dylans-nirbytes-sucks-handmade-nolife-list.stevenzawaski.com/bare/',
+    bare:'/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
