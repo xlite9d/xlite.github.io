fb9818730d65a523a2a5dafe1bfff8fa9abc7128
Flix <flixgit@outlook.com>
2025-05-13 18:34:46 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,22 @@
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-      caches.open("app-cache").then((cache) => {
-        return cache.addAll(["/v1oper-a.html", "/style.css", "/script.js"]);
+      caches.open('vplaza-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/script.js',
+          '/icon-192x192.png',
+          '/icon-512x512.png',
+        ]);
       })
     );
   });
   
-  self.addEventListener("fetch", (event) => {
+  self.addEventListener('fetch', (event) => {
     event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
       })
     );
   });
