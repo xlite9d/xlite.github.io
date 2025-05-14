301bf89be2a6b2f2a1c06c0bf6d7dbb490a33f7a
Vplaza <159073116+1056516@users.noreply.github.com>
2025-05-14 01:34:15 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,9 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open('vplaza-cache').then((cache) => {
         return cache.addAll([
-          '/',
           '/index.html',
-          '/styles.css',
-          '/script.js',
-          '/icon-192x192.png',
-          '/icon-512x512.png',
+          '/vplazax.png',
+          '/vplazax.png',
         ]);
       })
     );
