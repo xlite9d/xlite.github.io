32c9556d4d1ed2e6d926abd935b3eb44b023b9ab
Vplaza <159073116+1056516@users.noreply.github.com>
2025-05-29 02:28:10 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
 	prefix: "/static/r/",
-	bare: "https://bare.cbgames.org/",
+	bare: "/bare",
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
 	handler: "/static/uv/uv.handler.js",
